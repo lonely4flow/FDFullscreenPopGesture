16f7e420d29edb2063beb5db03f5cdc314e63733
独孤流 <47106210+lonely4flow@users.noreply.github.com>
2019-08-10 11:29:22 +0800

    Update UINavigationController+FDFullscreenPopGesture.h
    添加对设置侧滑位置的响应

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h
@@ -55,8 +55,7 @@
 /// Default to NO, bars are more likely to show.
 @property (nonatomic, assign) BOOL fd_prefersNavigationBarHidden;
 
-/// Max allowed initial distance to left edge when you begin the interactive pop
-/// gesture. 0 by default, which means it will ignore this limit.
-@property (nonatomic, assign) CGFloat fd_interactivePopMaxAllowedInitialDistanceToLeftEdge;
+// 可侧滑交互的距离左侧的位置
+@property (nonatomic, assign) CGFloat fd_interactivePopLeftDistance;
 
 @end
